3b138cbedef1d9bb1ce562c2b638295402bf52ac
Hanjise0ng <tjdwl3269@gmail.com>
2024-12-23 15:31:14 +0900

    work4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,16 +12,27 @@ class App {
     public void run() {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
+        int idx = 1;
+        while(true) {
+            System.out.print("명령) ");
+            String options = sc.nextLine();
 
-        System.out.println("명령) 등록");
-        
-        System.out.print("명언 : ");
-        String wise = sc.nextLine();
+            if (options.equals("종료")) {
+                break;
+            }
+            else if (options.equals("등록")) {
+                System.out.print("명언 : ");
+                String wise = sc.nextLine();
 
-        System.out.print("작가 : ");
-        String name = sc.nextLine();
+                System.out.print("작가 : ");
+                String name = sc.nextLine();
 
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
+                System.out.println( idx++ + "번 명언이 등록되었습니다.");
+            }
+            else {
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+
     }
 }
