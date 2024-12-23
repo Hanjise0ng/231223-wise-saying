155c0c7a80a704d8cbe9f669ca9274f8b7de3a30
Hanjise0ng <tjdwl3269@gmail.com>
2024-12-23 15:56:43 +0900

    work5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +11,7 @@ public class Main {
 
 class App {
     public void run() {
+        ArrayList<WiseSaying> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         int idx = 1;
@@ -19,20 +21,43 @@ class App {
 
             if (options.equals("종료")) {
                 break;
-            }
-            else if (options.equals("등록")) {
+
+            } else if (options.equals("등록")) {
                 System.out.print("명언 : ");
-                String wise = sc.nextLine();
+                String sentence = sc.nextLine();
 
                 System.out.print("작가 : ");
-                String name = sc.nextLine();
-
+                String author = sc.nextLine();
+                WiseSaying w = new WiseSaying(idx, sentence, author);
+                list.add(w);
                 System.out.println( idx++ + "번 명언이 등록되었습니다.");
-            }
-            else {
+
+            } else if (options.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-------------------------------");
+
+                for (WiseSaying l : list) {
+                    System.out.println(l.idx + " / " + l.author + " / " + l.sentence);
+                }
+
+            } else {
                 System.out.println("잘못된 입력입니다.");
+
             }
         }
 
     }
+}
+
+class WiseSaying {
+    int idx;
+    String sentence;
+    String author;
+
+    WiseSaying(int idx, String sentence, String author) {
+        this.idx = idx;
+        this.sentence = sentence;
+        this.author = author;
+    }
+
 }
