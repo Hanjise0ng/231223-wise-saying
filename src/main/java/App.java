60890177449f8fca3a48805307dc47c48613f8ca
Hanjise0ng <tjdwl3269@gmail.com>
2024-12-24 15:18:13 +0900

    work10

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,13 +26,23 @@ public class App {
             } else if (command.equals("목록")) {
                 printWiseSayingList();
             } else if(command.startsWith("삭제?id=")) {
-                deleteWiseSaying();
+
+                String strId = command.substring(6);
+                int id = Integer.parseInt(strId);
+
+                deleteWiseSaying(id);
+                System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
             }
         }
     }
-
-    private void deleteWiseSaying() {
-        System.out.println("삭제");
+    // [a:0, b:1, c:2]
+    private void deleteWiseSaying(int targetId) {
+        for(WiseSaying wiseSaying : wiseSayingList) {
+            if(wiseSaying.getId() == targetId) {
+                wiseSayingList.remove(wiseSaying);
+                break;
+            }
+        }
     }
 
     private void printWiseSayingList() {
